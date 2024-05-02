ab2434b436d625a7ffe7a65eee5ebe52177bb60c
ganolan <gnolz1@gmail.com>
2024-05-02 11:20:55 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,11 +5,12 @@ import cohere
 # Adapted from the StreamLit OpenAI Chatbot example - https://github.com/streamlit/llm-examples/blob/main/Chatbot.py
 
 with st.sidebar:
-    if 'COHERE_API_KEY' in st.secrets:
-        cohere_api_key = st.text_input("Cohere API Key", key="chatbot_api_key", type="password")
-        "[Get a Cohere API Key](https://dashboard.cohere.ai/api-keys)"
+    if hasattr(st, 'secrets'):
+        if 'COHERE_API_KEY' in st.secrets:
+            cohere_api_key = st.secrets['COHERE_API_KEY']
     else:
-        cohere_api_key = st.secrets['COHERE_API_KEY']
+        cohere_api_key = st.text_input("Cohere API Key", key="chatbot_api_key", type="password")
+        st.markdown("[Get a Cohere API Key](https://dashboard.cohere.ai/api-keys)")
 
 st.title("💬 My First Chatbot")
 
